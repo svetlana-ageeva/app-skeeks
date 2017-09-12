2a29c2be9d4ac26e5bec425018ab762bc6a4f254
Ekilei <ekilei@gmail.com>
2017-09-12 16:00:19 +0300

    вывод цены

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -8,7 +8,8 @@
 
 namespace frontend\controllers;
 
-
+use skeeks\cms\models\CmsContentElement;
+use Yii;
 use yii\web\Controller;
 
 class SearchController extends Controller
@@ -20,7 +21,24 @@ class SearchController extends Controller
     {
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
 
-        return ['USA','RUS'];
+        $term = Yii::$app->request->get('term');
+
+        $products = CmsContentElement::find()->where(['like','name',$term])->all();
+
+        $arr = [];
+        if($products)
+        {
+            /**
+             * @var \skeeks\cms\models\CmsContentElement $model
+             */
+            foreach ($products as $model)
+            {
+                $shopProduct = \skeeks\cms\shop\models\ShopProduct::getInstanceByContentElement($model);
+                $arr[] = $model->name.' - '. \Yii::$app->money->convertAndFormat($shopProduct->baseProductPrice->money);
+            }
+        }
+
+        return $arr;
     }
 
 }
